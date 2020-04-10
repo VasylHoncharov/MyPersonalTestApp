73a79da715e1fc3a382f33fadfab5d2f067b0340
Vasyl Honcharov <vga77m@gmail.com>
2020-04-10 17:18:55 +0300

    Added TestApp, MainIcon & packages

---
+++ app/src/main/java/com/vgsoft/mypersonaltestapp/TestApp.java
@@ -0,0 +1,35 @@
+package com.vgsoft.mypersonaltestapp;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+
+public class TestApp extends Application {
+    private static TestApp singleton;
+    private static Context context;
+    private static Resources resources;
+
+// Возвращает экземпляр данного класса
+
+    public static TestApp getInstance() {
+        return singleton;
+    }
+
+    @Override
+
+    public final void onCreate() {
+        super.onCreate();
+        singleton = this;
+        TestApp.context = getApplicationContext();
+        resources = getResources();
+    }
+
+    public static Context getAppContext() {
+        return TestApp.context;
+    }
+
+    public static Resources getRes() {
+        return resources;
+    }
+}
--- app/src/main/java/com/vgsoft/mypersonaltestapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/vgsoft/mypersonaltestapp/ui/activity/MainActivity.java
@@ -1,8 +1,11 @@
-package com.vgsoft.mypersonaltestapp;
+package com.vgsoft.mypersonaltestapp.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.vgsoft.mypersonaltestapp.R;
+import com.vgsoft.mypersonaltestapp.TestApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestApp.getRes().getString(R.string.app_name);
+
     }
 }
